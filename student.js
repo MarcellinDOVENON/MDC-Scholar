b42b84e02572804304d54af369c5326402e48965
MarcellinDOVENON <marcepepe@gmail.com>
2026-08-11 22:32:42 +0100

    Delete student.js

--- student.js
+++
@@ -1,9 +0,0 @@
-﻿export function initStudentForm() {
-    const bouton = document.getElementById('mybutton');
-    if (bouton) {
-        bouton.addEventListener('click', function(event) {
-            event.preventDefault();
-            // Logique de recherche à ajouter ici
-        });
-    }
-}
