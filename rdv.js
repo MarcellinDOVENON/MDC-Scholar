d7b066aad77c4231f1597aec317f72375257c12c
MarcellinDOVENON <marcepepe@gmail.com>
2026-08-11 22:33:02 +0100

    Delete rdv.js

--- rdv.js
+++
@@ -1,9 +0,0 @@
-﻿export function initRdvForm() {
-    const bouton2 = document.getElementById('mybutton2');
-    if (bouton2) {
-        bouton2.addEventListener('click', function(event) {
-            event.preventDefault();
-            alert('Demande envoyée !');
-        });
-    }
-}
