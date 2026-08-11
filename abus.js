adb470129cbaf94efbb52145b088f034192bb962
MarcellinDOVENON <marcepepe@gmail.com>
2026-08-11 22:33:49 +0100

    Delete abus.js

--- abus.js
+++
@@ -1,9 +0,0 @@
-﻿export function initAbusForm() {
-    const bouton3 = document.getElementById('mybutton3');
-    if (bouton3) {
-        bouton3.addEventListener('click', function(event) {
-            event.preventDefault();
-            alert('Votre dénonciation a été prise en compte. Nous vous recontacterons bientôt.');
-        });
-    }
-}
